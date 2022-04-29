9eb3b98dcb34079744e6319cf2bf4cba0092de78
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-04-29 10:52:43 -0400

    remove client subtree.

--- view/src/forms/index.js
+++
@@ -1,5 +0,0 @@
-import FormInput from './FormInput/FormInput';
-import LoginForm from './LoginForm/LoginForm';
-import SignupForm from './SignupForm/SignupForm';
-
-export { FormInput, LoginForm, SignupForm };
