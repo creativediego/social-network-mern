01008c76b289c3f96d11212b1e28b0456ba8d027
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-04-29 12:09:38 -0400

    Fix client path.

--- server.ts
+++ server.ts
@@ -16,9 +16,9 @@ handleUncaughtException();
 if (process.env.NODE_ENV! === 'PRODUCTION') {
   app.set('trust proxy', 1); // trust first proxy
   app.use(express.static(path.join(__dirname, 'view/build')));
-  app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, 'view/build', 'index.html'));
-  });
+  // app.get('*', (req: Request, res: Response) => {
+  //   res.sendFile(path.resolve(__dirname, 'view/build', 'index.html'));
+  // });
 }
 
 httpServer.listen(process.env.PORT! || 4000, () => {
