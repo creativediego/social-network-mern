640177ba0e2661c661bec706b2be4c986e99398b
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-04-29 11:04:44 -0400

    remove view subtree.

--- view/src/views/ExploreView/ExploreView.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-const ExploreView = () => {
-  return <h1>Explore Screen</h1>;
-};
-export default ExploreView;
--- view/src/views/ListsView/ListsView.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-const ListsView = () => {
-  return <h1>Lists Screen</h1>;
-};
-export default ListsView;
