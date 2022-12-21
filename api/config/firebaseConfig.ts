41ca090e61f884be4fd8588e85ce5860632d91c2
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-12-20 23:02:30 -0500

    Edit firebase config.

--- api/config/firebaseConfig.ts
+++ api/config/firebaseConfig.ts
@@ -1,8 +1,9 @@
 import * as admin from 'firebase-admin';
-const serviceAccount = require('./serviceAccount.json');
+// const serviceAccount = require('./serviceAccount.json');
+import credentials from './serviceAccount';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(credentials),
 });
 
 export default admin;
