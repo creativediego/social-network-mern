73bbac55823bcb9e5badd53f85312ba1d7ebf4cf
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-12-23 15:12:25 -0500

    Edit server config.

--- server.ts
+++ server.ts
@@ -17,15 +17,15 @@ createControllers(app);
 handleUncaughtException();
 app.use(handleCentralError);
 
-if (process.env.NODE_ENV! === 'production') {
-  app.set('trust proxy', 1); // trust first proxy
-  // app.use(express.static(path.join(__dirname, '../client/build'))); // serve react app
-  app.use('/static', express.static(path.join(__dirname, '../client/build')));
-  // app.use(express.static(__dirname + '/client/build'));
-  // app.get('*', (req: Request, res: Response) => {
-  //   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  // });
-}
+// if (process.env.NODE_ENV! === 'production') {
+app.set('trust proxy', 1); // trust first proxy
+app.use(express.static(path.join(__dirname, '../client/build'))); // serve react app
+app.get('*', (req: Request, res: Response) => {
+  const reactPath = path.resolve(__dirname, '../client/build', 'index.html');
+  console.log(reactPath);
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+// }
 
 httpServer.listen(process.env.PORT! || 4000, () => {
   console.log(`Up and running on port: ${process.env.PORT! || 4000}`);
